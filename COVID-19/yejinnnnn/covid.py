c855ca0c383cb1fb5c01941fa91a4778b7aeaaa5
Yejin Kim <yejinkim@Yejinui-MacBookPro.local>
2020-09-06 12:12:59 +0900

    텍스트 내 COVID-19 등장 횟수 카운트하

---
+++ COVID-19/yejinnnnn/covid.py
@@ -0,0 +1,8 @@
+file = open("00.txt", "r")
+text = file.read()
+words = text.split()
+wordCount = dict()
+for word in words:
+    wordCount[word] = wordCount.get(word, 0) + 1
+for word, count in wordCount.items():
+    pritn(word, count)
