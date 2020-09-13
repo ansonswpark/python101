986d67756cf06de667eb9c18956f5862ed2088b2
brian0929-star <a01088640446@gmail.com>
2020-09-13 12:37:38 +0900

    COVID-19 개수 세기

--- COVID-19/brian0929-star/covid1.py
+++ COVID-19/brian0929-star/covid1.py
@@ -2,9 +2,9 @@ import glob
 news_list = glob.glob("C:/Users/marsg/python101/COVID-19/news/*.txt")
 
 total_count = 0
-
+        
 for news in news_list:
-    with open(news, "r", encoding='UTF8') as file:
+      with open(news, "r", encoding='UTF8') as file:
         contents = file.read()
         total_count += contents.count('COVID-19')
 
