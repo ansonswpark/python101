0804d1a865bf307fd5e33d86b44469520d8a7b91
dokyung1201 <geniusbrandonlee@gmail.com>
2020-09-13 12:43:20 +0900

    covid-19 count

--- COVID-19/dokyung1201/covid.py
+++ COVID-19/dokyung1201/covid.py
@@ -1,3 +1,10 @@
-with open("00.txt", "r") as file:
-    contents = file.read()
-print(contents)
+import glob
+news_files = glob.glob("C:/Users/user/Documents/python101/COVID-19/news/*.txt")
+total_count = 0
+for news_file in news_files:
+    with open(news_file, "r", encoding='UTF-8') as f:
+        contents = f.read()
+        count = contents.count("COVID-19")
+        total_count += count
+
+print(total_count)
