5e4e5d15f72a14ed48cf6a18862ed1bb0050aeee
Yejin Kim <yejin@deepnatural.ai>
2020-09-13 12:32:25 +0900

    뉴스 텍스트 내 COVID-19 단어 빈도수 추출

--- COVID-19/yejinnnnn/covid.py
+++ COVID-19/yejinnnnn/covid.py
@@ -1,13 +1,19 @@
-file = open("00.txt", "r")
-text = file.read()
-words = text.split()
-wordCount = dict()
-for word in words:
-    wordCount[word] = wordCount.get(word, 0) + 1
-for word, count in wordCount.items():
-    pritn(word, count)
+import glob
+news_list = glob.glob("COVID-19/news/*.txt")
+total_count = 0
+
+for news in news_list:
+    with open(news, "r") as file:
+        contents = file.read()
+    total_count += contents.count("COVID-19")
+print(total_count)
 
 
-
-
-    
+# file = open("00.txt", "r")
+# text = file.read()
+# words = text.split()
+# wordCount = dict()
+# for word in words:
+#     wordCount[word] = wordCount.get(word, 0) + 1
+# for word, count in wordCount.items():
+#     print(word, count)
